691ddbb6c3d63cd7bc81f32ea1545480a3847eaf
aapoorv2 <abhyudaya.a_ftc@external.swiggy.in>
2024-01-24 10:21:33 +0530

    update temperature

--- src/main/java/Temperature.java
+++ src/main/java/Temperature.java
@@ -14,9 +14,15 @@ public class Temperature {
             return false;
         }
         Temperature temp2 = (Temperature) o;
+
         double val1 = unit.convertToBase(value);
         double val2 = temp2.unit.convertToBase(temp2.value);
-        return Double.compare(val1, val2) == 0;
+        String formattedValue1 = String.format("%.2f", val1);
+        double roundedValue1 = Double.parseDouble(formattedValue1);
+        String formattedValue2 = String.format("%.2f", val2);
+        double roundedValue2 = Double.parseDouble(formattedValue2);
+
+        return Double.compare(roundedValue1, roundedValue2) == 0;
     }
 
 }
